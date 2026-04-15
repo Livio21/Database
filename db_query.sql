361902186d74c697c95e41df15a5d8ddb30d3c65
Livio21 <livio.macaj@cit.edu.al>
2026-04-15 16:21:16 +0200

    Shtimi i queries per 10 kurset dhe komente per queries

--- db_query.sql
+++ db_query.sql
@@ -1,3 +1,4 @@
+-- krijimi i tabeles kursi
 create table kursi(
     id SERIAL primary key ,
     emri_kursit VARCHAR(100),
@@ -6,12 +7,54 @@ create table kursi(
     update_date DATE
 );
 
-insert into kursi (emri_kursit, create_date, update_date, kohezgjatja) VALUES ('Java','10/10/2023','10/10/2023',3);
+-- Insert queries per 10 kurse
+insert into kursi (emri_kursit, create_date, update_date, kohezgjatja) values ('Java','10/10/2023','10/10/2023',3);
 
+insert into kursi (emri_kursit, create_date, update_date, kohezgjatja) values ('Python','01/02/2023','05/02/2023',4);
+
+insert into kursi (emri_kursit, create_date, update_date, kohezgjatja) values ('JavaScript','12/06/2023','15/06/2023',2);
+
+insert into kursi (emri_kursit, create_date, update_date, kohezgjatja) values ('C++','20/11/2023','25/11/2023',5);
+
+insert into kursi (emri_kursit, create_date, update_date, kohezgjatja) values ('React','05/03/2024','10/03/2024',3);
+
+insert into kursi (emri_kursit, create_date, update_date, kohezgjatja) values ('Angular','18/07/2024','20/07/2024',4);
+
+insert into kursi (emri_kursit, create_date, update_date, kohezgjatja) values ('Node.js','10/09/2024','12/09/2024',3);
+
+insert into kursi (emri_kursit, create_date, update_date, kohezgjatja) values ('Flutter','01/02/2025','05/02/2025',5);
+
+insert into kursi (emri_kursit, create_date, update_date, kohezgjatja) values ('Flutter','01/02/2025','05/02/2025',5);
+
+insert into kursi (emri_kursit, create_date, update_date, kohezgjatja) values ('Machine Learning','01/10/2025','05/10/2025',8);
+
+-- Shtimi i kolones programming_language ne tabelen kursi
 alter table kursi add column programming_language VARCHAR(100);
+
+-- Update queries per ndryshimin programming_language bazuar ne emri_kursit
+update kursi set programming_language='Java' where emri_kursit like 'Java%';
+
+update kursi set programming_language='Python' where emri_kursit like 'Python%';
+
+update kursi set programming_language='JavaScript' where emri_kursit like 'JavaScript%';
+
+update kursi set programming_language='C++' where emri_kursit like 'C++%';
+
+update kursi set programming_language='TypeScript' where emri_kursit like 'Angular%';
+
+update kursi set programming_language='Java' where emri_kursit like 'Spring%';
+
+update kursi set programming_language='JavaScript' where emri_kursit like 'Node%';
+
+update kursi set programming_language='Dart' where emri_kursit like 'Flutter%';
+
+update kursi set programming_language='R' where emri_kursit like 'Data%';
+
+update kursi set programming_language='Python' where emri_kursit like 'Machine%';
 
 update kursi set programming_language='Java' where emri_kursit like 'Java%';
 
+-- krijimi i tabeles student me foreign key kursi_id qe referon id e tabeles kursi
 create table student (
     id serial primary key ,
     emri varchar(100),
@@ -23,12 +66,18 @@ create table student (
 
 );
 
+--insert studentet
 insert into student (emri, email, birth_date, phone_number, pike, kursi_id) VALUES ('Student Student','student@email.com','01/01/2001','0681231234',50, 1);
 
+insert into student (emri, email, birth_date, phone_number, pike, kursi_id) VALUES ('A A','student@email.com','01/01/2001','0681231234',50, 1);
+
+--ndryshimi i kolones pike ne points
 alter table student rename column pike to points;
 
+-- per te treguar te gjithe studentet qe ju fillon emri me shkronjen A
 select * from student where emri like 'A%';
 
 select * from student where emri like 'S%';
 
+-- per te treguar te gjithe studentet qe jane mbi 25 vjec
 select * from  student where ( date_part('year',current_date) - date_part('year', birth_date) >= 25);
